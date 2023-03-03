50b49c966e350fc359386b78fb12ba30f4ab0b53
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-03-03 19:13:25 +0300

    chore: FileWatcher implemented to main

--- src/main/java/org/gucardev/Main.java
+++ src/main/java/org/gucardev/Main.java
@@ -1,6 +1,20 @@
 package org.gucardev;
 
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import org.gucardev.util.FileWatcher;
+
 public class Main {
 
-  public static void main(String[] args) {}
+  public static void main(String[] args) {
+    Path path = Paths.get(
+        "D:\\SoftwareProjects\\hicx-case\\src\\main\\resources\\watch_this_directory");
+    try {
+
+      Thread thread = new Thread(new FileWatcher(path));
+      thread.start();
+    } catch (Exception e) {
+      System.out.println("Error creating file watcher: " + e.getMessage());
+    }
+  }
 }

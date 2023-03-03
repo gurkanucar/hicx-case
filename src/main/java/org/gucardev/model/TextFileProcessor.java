ceb1e734e5d8ff9ea9be4d5413d41c45cb88187a
Gurkan UCAR <ucar.gurkan@hotmail.com>
2023-03-03 20:45:35 +0300

    chore: methods implemented in TextFileProcessor

--- src/main/java/org/gucardev/model/TextFileProcessor.java
+++ src/main/java/org/gucardev/model/TextFileProcessor.java
@@ -1,13 +1,49 @@
 package org.gucardev.model;
 
+import java.io.IOException;
+import org.gucardev.util.FileUtil;
+import org.gucardev.util.StatisticsUtil;
+import org.gucardev.util.TextFileReader;
+
 public class TextFileProcessor extends BaseFileProcessor {
 
-  @Override
-  public void calculateNumberOfWords() {}
+  public TextFileProcessor(String path) {
+    super(path);
+    super.statistic = new Statistic();
+    super.fileName = FileUtil.getFileName(path);
+    super.extension = FileUtil.getFileExtension(path);
+    super.size = FileUtil.getFileSize(path);
+    super.created = FileUtil.getFileCreatedDate(path);
+  }
 
   @Override
-  public void calculateNumberOfDots() {}
+  public void calculateNumberOfWords() {
+    try {
+      long numberOfWords = StatisticsUtil.calculateNumberOfWords(TextFileReader.read(this.path));
+      this.getStatistic().setNumberOfWords(numberOfWords);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 
   @Override
-  public void calculateMostUsedWord() {}
+  public void calculateNumberOfDots() {
+    try {
+      long numberOfDots =
+          StatisticsUtil.calcNumberOfSpecificChar(TextFileReader.read(this.path), '.');
+      this.getStatistic().setNumberOfDots(numberOfDots);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
+
+  @Override
+  public void calculateMostUsedWord() {
+    try {
+      String mostUsedWord = StatisticsUtil.calculateMostUsedWord(TextFileReader.read(this.path));
+      this.getStatistic().setMostUsedWord(mostUsedWord);
+    } catch (IOException e) {
+      throw new RuntimeException(e);
+    }
+  }
 }
